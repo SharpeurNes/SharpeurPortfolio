e0a638f38e7bdd4d4257904cf53f31a87dc1ed29
SharpeurNes <sharpeurnes@gmail.com>
2025-07-05 07:46:38 +0200

    fix build fail vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,1 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import mkcert from 'vite-plugin-mkcert'
 
-// https://astro.build/config
-export default defineConfig({
-	output: 'server',
-	vite: {
-		plugins: [ mkcert() ]
-	},
-});
