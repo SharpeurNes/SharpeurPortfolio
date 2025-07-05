8533818e46ce1ddf8505e194a2ee014c6749797c
SharpeurNes <sharpeurnes@gmail.com>
2025-07-05 07:40:22 +0200

    Update About page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mkcert from 'vite-plugin-mkcert'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	output: 'server',
+	vite: {
+		plugins: [ mkcert() ]
+	},
+});
